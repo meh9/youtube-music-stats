823d5b13a3ecc8b35bce600b920f8751e37e79b8
Mats Henrikson <mats@zakalwe.com>
2023-05-18 17:35:20 +1000

    Added writing to a CSV file.

--- listen_history.py
+++ listen_history.py
@@ -1,3 +1,4 @@
+import csv
 import json
  
 f = open("watch-history.json", encoding="utf8")
@@ -5,26 +6,27 @@ f = open("watch-history.json", encoding="utf8")
 data = json.load(f)
 pruned = {}
  
-music = True
- 
 for entry in data:
-    if ((entry["header"] == "YouTube Music") == music) and (entry["title"] != "Visited YouTube Music") and ("https://www.youtube.com/watch?v=" not in entry["title"]) and (entry["title"] != "Watched a video that has been removed"):
+    if (entry["header"] == "YouTube Music") and (entry["title"] != "Visited YouTube Music") and ("https://www.youtube.com/watch?v=" not in entry["title"]) and (entry["title"] != "Watched a video that has been removed"):
         
-        title   = entry["title"][8::]
+        title = entry["title"][8::]
  
-        if music:
-            artist  = entry["subtitles"][0]["name"].replace(" - Topic", "")
-            title = f'{artist} - {title}'
+        artist = entry["subtitles"][0]["name"].replace(" - Topic", "")
+        artist_title = (artist, title)
  
-        if title in pruned:
-            pruned[title] = pruned[title] + 1
+        if artist_title in pruned:
+            pruned[artist_title] = pruned[artist_title] + 1
         else:
-            pruned[title] = 1
+            pruned[artist_title] = 1
  
 total_plays = 0
  
-for song, listens in dict(sorted(pruned.items(), key=lambda item: item[1])).items():
-    total_plays += listens
-    print(f'{song} : {listens}')
+with open("history.csv", "w", newline="") as csvfile:
+    history_writer = csv.writer(csvfile, dialect="excel", quoting=csv.QUOTE_MINIMAL)
+    history_writer.writerow(["Artist", "Title", "Listens"])
+
+    for song, listens in dict(sorted(pruned.items(), key=lambda item: item[1], reverse=True)).items():
+        total_plays += listens
+        history_writer.writerow([song[0], song[1], listens])
  
 print(f'{total_plays} total plays across all tracks')
