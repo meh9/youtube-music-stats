f28b58902d16af928c51f3052efec1c7632a79c7
Mats Henrikson <mats@zakalwe.com>
2023-05-18 17:57:52 +1000

    Types, with for loading the json, lots of other good stuff.

--- listen_history.py
+++ listen_history.py
@@ -1,32 +1,45 @@
+"""
+Parse watch-history.json from Google Takeout, count how many times each song has been played, 
+and write the stats to a CSV.
+"""
+
 import csv
 import json
- 
-f = open("watch-history.json", encoding="utf8")
- 
-data = json.load(f)
-pruned = {}
- 
+from typing import Any
+
+data: list[dict[str, Any]] = []
+pruned: dict[tuple[str, str], int] = {}
+
+with open("watch-history.json", "r", encoding="utf8") as f:
+    data = json.load(f)
+
 for entry in data:
-    if (entry["header"] == "YouTube Music") and (entry["title"] != "Visited YouTube Music") and ("https://www.youtube.com/watch?v=" not in entry["title"]) and (entry["title"] != "Watched a video that has been removed"):
-        
-        title = entry["title"][8::]
- 
-        artist = entry["subtitles"][0]["name"].replace(" - Topic", "")
-        artist_title = (artist, title)
- 
+    if (
+        (entry["header"] == "YouTube Music")
+        and (entry["title"] != "Visited YouTube Music")
+        and ("https://www.youtube.com/watch?v=" not in entry["title"])
+        and (entry["title"] != "Watched a video that has been removed")
+    ):
+        title: str = entry["title"][8::]
+
+        artist: str = entry["subtitles"][0]["name"].replace(" - Topic", "")
+        artist_title: tuple[str, str] = (artist, title)
+
         if artist_title in pruned:
             pruned[artist_title] = pruned[artist_title] + 1
         else:
             pruned[artist_title] = 1
- 
-total_plays = 0
- 
-with open("history.csv", "w", newline="") as csvfile:
+
+total_plays: int = 0
+
+with open("history.csv", "w", encoding="utf8") as csvfile:
     history_writer = csv.writer(csvfile, dialect="excel", quoting=csv.QUOTE_MINIMAL)
     history_writer.writerow(["Artist", "Title", "Listens"])
 
-    for song, listens in dict(sorted(pruned.items(), key=lambda item: item[1], reverse=True)).items():
+    for song, listens in dict(
+        sorted(pruned.items(), key=lambda item: item[1], reverse=True)
+    ).items():
         total_plays += listens
         history_writer.writerow([song[0], song[1], listens])
- 
-print(f'{total_plays} total plays across all tracks')
+
+print(f"{total_plays} total plays across all tracks")
